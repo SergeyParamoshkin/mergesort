912c32cecfe6278f3e38cb996cecb26d8ef95b1c
Sergey <serg3091@gmail.com>
2022-05-11 14:32:23 +0300

    fix

--- main.go
+++ main.go
@@ -9,23 +9,61 @@ const (
 	bound = 10000
 )
 
-func generator(size, divisor int64) []int64 {
-	rnd := rand.New(rand.NewSource(size + 42))
-	prev := rnd.Int63n(size)
-	// rands := make([]int64, size)
-	rands := []int64{}
-	for i := int64(0); len(rands) < int(size); i++ {
-		if d := ((prev + 1) + rnd.Int63n(bound)); d&int64(1) == divisor {
-			rands = append(rands, d)
-			prev = d
-		}
-	}
+func generator(size, divisor int64) chan int64 {
 
-	return rands
+	// TODO: migrate to crypto
+
+	c := make(chan int64, size)
+
+	go func() {
+		defer close(c)
+
+		rnd := rand.New(rand.NewSource(size + 42))
+		prev := rnd.Int63n(size)
+
+		for i := int64(0); len(c) < int(size); i++ {
+			if d := prev + rnd.Int63n(bound); d&int64(1) == divisor {
+				c <- d
+				prev = d
+			}
+		}
+	}()
+
+	return c
 }
 
 func main() {
-	fmt.Println(generator(10, 0))
-	fmt.Println(generator(10, 1))
+	g1 := generator(10, 0)
+	g2 := generator(10, 1)
 
+	v1, _ := <-g1
+	v2, _ := <-g2
+	ok := true
+	for g1 != nil || g2 != nil {
+		if g1 == nil {
+			fmt.Println(v2)
+			v2, ok = <-g2
+			if !ok {
+				g2 = nil
+			}
+		} else if g2 == nil {
+			fmt.Println(v1)
+			v1, ok = <-g1
+			if !ok {
+				g1 = nil
+			}
+		} else if v1 < v2 {
+			fmt.Println(v1)
+			v1, ok = <-g1
+			if !ok {
+				g1 = nil
+			}
+		} else {
+			fmt.Println(v2)
+			v2, ok = <-g2
+			if !ok {
+				g2 = nil
+			}
+		}
+	}
 }
